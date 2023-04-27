9dd9af7b5a43ff9677b4e7272b629d32acfdc9b5
Carlos Burgos <cburgos@smc.cl>
2023-04-27 13:41:13 -0400

    prueba bt

--- BlueBlazor/Program.cs
+++ BlueBlazor/Program.cs
@@ -2,10 +2,12 @@ using BlueBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Add Bluetooth support
 
 await builder.Build().RunAsync();
