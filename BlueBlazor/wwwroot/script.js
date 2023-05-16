ca4f2e2e8066d28e5a9b628283fdca09b58119c4
Carlos Burgos <cburgos@smc.cl>
2023-05-16 09:45:30 -0400

    print demo

--- BlueBlazor/wwwroot/script.js
+++ BlueBlazor/wwwroot/script.js
@@ -1,3 +1,20 @@
 ﻿function printInvoke() {
     window.print();
 }
+
+async function printText(text) {
+    try {
+        const options = {
+            filters: [{ services: ['bluetooth_printer_service'] }]
+        };
+        const device = await navigator.bluetooth.requestDevice(options);
+        const server = await device.gatt.connect();
+        const service = await server.getPrimaryService('bluetooth_printer_service');
+        const characteristic = await service.getCharacteristic('bluetooth_printer_characteristic');
+        await characteristic.writeValue(new TextEncoder().encode(text));
+        await server.disconnect();
+        console.log('Texto enviado a la impresora');
+    } catch (error) {
+        console.error(error);
+    }
+}
